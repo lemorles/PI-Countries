6f28792ba2ff8d2666ff4507ee2ae9118fd1451c
Leandro <leomoralesarce@gmail.com>
2022-04-06 09:30:36 -0300

    restructuring action types

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,17 +1,44 @@
-export const GET_COUNTRIES = "GET_COUNTRIES";
-export const GET_COUNTRY = "GET_COUNTRY";
-export const GET_ACTIVITIES = "GET_ACTIVITIES";
-export const CREATE_ACTIVITY = "CREATE_ACTIVITY";
+export const GET_COUNTRIES_PENDING = "GET_COUNTRIES_PENDING";
+export const GET_COUNTRIES_SUCCESS = "GET_COUNTRIES_SUCCESS";
+export const GET_COUNTRIES_ERROR = "GET_COUNTRIES_ERROR";
+
+export const GET_COUNTRY_PENDING = "GET_COUNTRY_PENDING";
+export const GET_COUNTRY_SUCCESS = "GET_COUNTRY_SUCCESS";
+export const GET_COUNTRY_ERROR = "GET_COUNTRY_ERROR";
+
+export const GET_ACTIVITIES_PENDING = "GET_ACTIVITIES_PENDING";
+export const GET_ACTIVITIES_SUCCESS = "GET_ACTIVITIES_SUCCESS";
+export const GET_ACTIVITIES_ERROR = "GET_ACTIVITIES_ERROR";
+
+export const CREATE_ACTIVITY_PENDING = "CREATE_ACTIVITY_PENDING";
+export const CREATE_ACTIVITY_SUCCESS = "CREATE_ACTIVITY_SUCCESS";
+export const CREATE_ACTIVITY_ERROR = "CREATE_ACTIVITY_ERROR";
+
+export const FILTER_BY_NAME_PENDING = "FILTER_BY_NAME_PENDING";
+export const FILTER_BY_NAME_SUCCESS = "FILTER_BY_NAME_SUCCESS";
+export const FILTER_BY_NAME_ERROR = "FILTER_BY_NAME_ERROR";
+
+export const FILTER_BY_REGION = "FILTER_BY_REGION";
+export const FILTER_BY_ACTIVITY = "FILTER_BY_ACTIVITY";
+export const SORT_BY_NAME = "SORT_BY_NAME";
+export const SORT_BY_POPULATION = "SORT_BY_POPULATION";
 
 export const getCountries = () => {
   return async (dispatch) => {
     try {
+      dispatch({ type: GET_COUNTRIES_PENDING });
+
       const res = await fetch(`http://localhost:3001/countries`);
       const data = await res.json();
 
-      return dispatch({ type: GET_COUNTRIES, payload: data });
+      if (res.ok) {
+        dispatch({ type: GET_COUNTRIES_SUCCESS, payload: data });
+      } else {
+        dispatch({ type: GET_COUNTRIES_ERROR, payload: data });
+      }
     } catch (err) {
       console.log(err);
+      dispatch({ type: GET_COUNTRIES_ERROR, payload: err });
     }
   };
 };
@@ -19,12 +46,19 @@ export const getCountries = () => {
 export const getCountry = (id) => {
   return async (dispatch) => {
     try {
+      dispatch({ type: GET_COUNTRY_PENDING });
+
       const res = await fetch(`http://localhost:3001/countries/${id}`);
       const data = await res.json();
 
-      return dispatch({ type: GET_COUNTRY, payload: data });
+      if (res.status === 200) {
+        dispatch({ type: GET_COUNTRY_SUCCESS, payload: data });
+      } else {
+        dispatch({ type: GET_COUNTRY_ERROR, payload: data });
+      }
     } catch (err) {
       console.log(err);
+      dispatch({ type: GET_COUNTRY_ERROR, payload: err });
     }
   };
 };
@@ -32,12 +66,19 @@ export const getCountry = (id) => {
 export const getActivities = () => {
   return async (dispatch) => {
     try {
+      dispatch({ type: GET_ACTIVITIES_PENDING });
+
       const res = await fetch(`http://localhost:3001/activity`);
       const data = await res.json();
 
-      return dispatch({ type: GET_ACTIVITIES, payload: data });
+      if (res.status === 200) {
+        dispatch({ type: GET_ACTIVITIES_SUCCESS, payload: data });
+      } else {
+        dispatch({ type: GET_ACTIVITIES_ERROR, payload: data });
+      }
     } catch (err) {
       console.log(err);
+      return dispatch({ type: GET_ACTIVITIES_ERROR, payload: err });
     }
   };
 };
@@ -45,20 +86,69 @@ export const getActivities = () => {
 export const createActivity = (input) => {
   return async (dispatch) => {
     try {
-      const res = await fetch("http://localhost:3001/activity", {
+      dispatch({ type: CREATE_ACTIVITY_PENDING });
+
+      const options = {
         method: "POST",
-        headers: {
-          "content-Type": "application/json",
-        },
+        headers: { "content-Type": "application/json" },
         body: JSON.stringify(input),
-      });
+      };
+
+      const res = await fetch("http://localhost:3001/activity", options);
       const data = await res.json();
 
-      return dispatch({
-        type: CREATE_ACTIVITY,
-      });
+      if (res.ok) {
+        dispatch({ type: CREATE_ACTIVITY_SUCCESS, payload: data });
+      } else {
+        dispatch({ type: CREATE_ACTIVITY_ERROR, payload: data });
+      }
     } catch (err) {
       console.log(err);
+      dispatch({ type: CREATE_ACTIVITY_ERROR, payload: err });
     }
+  };
+};
+
+export const filterCountriesByName = (query = "") => {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: FILTER_BY_NAME_PENDING });
+
+      const res = await fetch(`http://localhost:3001/countries?${query}`);
+      const data = await res.json();
+
+      dispatch({ type: FILTER_BY_NAME_SUCCESS, payload: data });
+    } catch (err) {
+      console.log(err);
+      return dispatch({ type: FILTER_BY_NAME_ERROR, payload: err });
+    }
+  };
+};
+
+export const filterCountriesByRegion = (value) => {
+  return {
+    type: FILTER_BY_REGION,
+    payload: value,
+  };
+};
+
+export const filterCountriesByActivity = (value) => {
+  return {
+    type: FILTER_BY_ACTIVITY,
+    payload: value,
+  };
+};
+
+export const sortCountriesByName = (value) => {
+  return {
+    type: SORT_BY_NAME,
+    payload: value,
+  };
+};
+
+export const sortCountriesByPopulation = (value) => {
+  return {
+    type: SORT_BY_POPULATION,
+    payload: value,
   };
 };
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,13 +1,23 @@
 import {
-  GET_COUNTRIES,
-  GET_COUNTRY,
-  GET_ACTIVITIES,
+  GET_COUNTRIES_PENDING,
+  GET_COUNTRIES_SUCCESS,
+  GET_COUNTRIES_ERROR,
+  GET_COUNTRY_PENDING,
+  GET_COUNTRY_SUCCESS,
+  GET_COUNTRY_ERROR,
+  GET_ACTIVITIES_PENDING,
+  GET_ACTIVITIES_SUCCESS,
+  GET_ACTIVITIES_ERROR,
+  CREATE_ACTIVITY_PENDING,
+  CREATE_ACTIVITY_SUCCESS,
+  CREATE_ACTIVITY_ERROR,
+  FILTER_BY_NAME_PENDING,
+  FILTER_BY_NAME_SUCCESS,
+  FILTER_BY_NAME_ERROR,
   FILTER_BY_REGION,
   FILTER_BY_ACTIVITY,
   SORT_BY_NAME,
   SORT_BY_POPULATION,
-  FILTER_BY_NAME,
-  CREATE_ACTIVITY,
 } from "../actions";
 
 const initialState = {
@@ -15,31 +25,181 @@ const initialState = {
   country: {},
   activities: [],
   filteredCountries: [],
+  loading: false,
+  error: {},
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_COUNTRIES:
+    case GET_COUNTRIES_PENDING:
+      return { ...state, loading: true };
+    case GET_COUNTRIES_SUCCESS:
       return {
         ...state,
+        loading: false,
         countries: action.payload,
         filteredCountries: action.payload,
       };
-    case GET_COUNTRY:
+    case GET_COUNTRIES_ERROR:
       return {
         ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case GET_COUNTRY_PENDING:
+      return { ...state, loading: true };
+    case GET_COUNTRY_SUCCESS:
+      return {
+        ...state,
+        loading: false,
         country: action.payload,
       };
-    case GET_ACTIVITIES:
+    case GET_COUNTRY_ERROR:
       return {
         ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case GET_ACTIVITIES_PENDING:
+      return { ...state, loading: true };
+    case GET_ACTIVITIES_SUCCESS:
+      return {
+        ...state,
+        loading: false,
         activities: action.payload,
       };
-    case CREATE_ACTIVITY:
+    case GET_ACTIVITIES_ERROR:
       return {
         ...state,
+        loading: false,
+        error: action.payload,
       };
+    case CREATE_ACTIVITY_PENDING:
+      return { ...state, loading: true };
+    case CREATE_ACTIVITY_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        // activities: action.payload,
+      };
+    case CREATE_ACTIVITY_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case FILTER_BY_NAME_PENDING:
+      return { ...state, loading: true };
+    case FILTER_BY_NAME_SUCCESS:
+      return {
+        ...state,
+        filteredCountries: action.payload,
+      };
+    case FILTER_BY_NAME_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case FILTER_BY_REGION:
+      if (action.payload === "") {
+        return { ...state, filteredCountries: state.countries };
+      } else {
+        // const newCountries = state.filteredCountries;
+        const newCountries = [...state.countries];
 
+        const filterCountries = newCountries.filter((c) => {
+          return c.region.toLowerCase().includes(action.payload.toLowerCase());
+        });
+
+        return {
+          ...state,
+          filteredCountries: filterCountries,
+        };
+      }
+    case FILTER_BY_ACTIVITY:
+      if (action.payload === "") {
+        return { ...state, filteredCountries: state.countries };
+      } else {
+        // const newCountries = state.filteredCountries;
+        const newCountries = [...state.countries];
+
+        const filterCountries = newCountries.filter((c) => {
+          return (
+            c.activities &&
+            c.activities.length &&
+            c.activities.find((a) => {
+              return a.name
+                .toLowerCase()
+                .includes(action.payload.toLowerCase());
+            })
+          );
+        });
+
+        return {
+          ...state,
+          filteredCountries: filterCountries,
+        };
+      }
+    case SORT_BY_NAME:
+      if (action.payload === "") {
+        return { ...state, filteredCountries: state.countries };
+      }
+
+      const newCountries = [...state.filteredCountries];
+
+      if (action.payload === "ASC") {
+        return {
+          ...state,
+          filteredCountries: newCountries.sort((a, b) => {
+            if (a.name > b.name) {
+              return 1;
+            }
+            if (a.name < b.name) {
+              return -1;
+            }
+
+            return 0;
+          }),
+        };
+      }
+
+      // DESC
+      return {
+        ...state,
+        filteredCountries: newCountries.sort((a, b) => {
+          if (a.name < b.name) {
+            return 1;
+          }
+          if (a.name > b.name) {
+            return -1;
+          }
+
+          return 0;
+        }),
+      };
+    case SORT_BY_POPULATION:
+      if (action.payload === "") {
+        return { ...state, filteredCountries: state.countries };
+      }
+
+      const newCountriess = [...state.filteredCountries];
+
+      if (action.payload === "ASC") {
+        return {
+          ...state,
+          filteredCountries: newCountriess.sort(
+            (a, b) => b.population - a.population
+          ),
+        };
+      }
+      // DESC
+      return {
+        ...state,
+        filteredCountries: newCountriess.sort(
+          (a, b) => a.population - b.population
+        ),
+      };
     default:
       return state;
   }
