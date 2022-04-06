3a615cb4b73d972e13140db8177e2baeefd31eac
Leandro <leomoralesarce@gmail.com>
2022-04-06 09:28:04 -0300

    add styles and functionality loading to component Country

--- client/src/components/Country/index.jsx
+++ client/src/components/Country/index.jsx
@@ -1,42 +1,73 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getCountry } from "../../actions";
+import Loader from "../Loader";
+import "./index.css";
 
 export default function Country() {
   const { id } = useParams();
   const dispatch = useDispatch();
-  const country = useSelector((state) => state.country);
+  const { country, loading } = useSelector((state) => state);
 
   useEffect(() => {
     dispatch(getCountry(id));
   }, [dispatch, id]);
 
+  if (loading) return <Loader />;
+
   return (
-    <div>
-      <p>{country.id}</p>
+    <div className="country-container">
+      {/* <p>{country.id}</p> */}
       <h1>{country.name}</h1>
-      <img src={country.flag} alt={country.name} />
-      <p>Capital:</p>
-      <ul>
-        {country.capitals &&
-          country.capitals.map((cap) => {
-            return <li key={cap.id}>{cap.name}</li>;
-          })}
-      </ul>
-      <p>Region: {country.region}</p>
-      <p>Subregion: {country.subregion}</p>
-      <p>
-        Area: {country.area} km<sup>2</sup>
-      </p>
-      <p>Population: {country.population}</p>
-      <p>Toutist Activities:</p>
-      <ul>
-        {country.activities &&
-          country.activities.map((act) => {
-            return <li key={act.id}>{act.name}</li>;
-          })}
-      </ul>
+      <div className="country-wrapper">
+        <img src={country.flag} alt={country.name} className="country-img" />
+        <div>
+          <ul className="capital-wrapper">
+            <p className="capitals">Capital:</p>
+            {country.capitals &&
+              country.capitals.map((cap) => {
+                return <li key={cap.id}>{cap.name}</li>;
+              })}
+          </ul>
+          <p>Region: {country.region}</p>
+          <p>Subregion: {country.subregion}</p>
+          <p>
+            Area: {country.area} km<sup>2</sup>
+          </p>
+          <p>Population: {country.population}</p>
+        </div>
+      </div>
+
+      {country.activities && country.activities.length ? (
+        <>
+          <h3>Tourist Activities:</h3>
+          <ul className="list-activity">
+            {country.activities.map((act) => {
+              return (
+                <li key={act.id} className="item-activity">
+                  <p>ACTIVITY {act.id}</p>
+                  <p>Name: {capitalizeFirstLetter(act.name)}</p>
+                  <p>Difficulty: {act.difficulty}</p>
+                  <p>Duration: {act.duration} hours</p>
+                  <p>Season: {capitalizeFirstLetter(act.season)}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </>
+      ) : (
+        <div>
+          <h3>No tourist activities found!</h3>
+          <NavLink to={"/activity"} className="btn-primary">
+            Create Activity
+          </NavLink>
+        </div>
+      )}
     </div>
   );
 }
+
+const capitalizeFirstLetter = (str) => {
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
+};
