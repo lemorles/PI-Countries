046810227200db1a987b2e768a3b368951bfe712
Leandro <leomoralesarce@gmail.com>
2022-04-07 11:52:22 -0300

    add name, region and activity filters in Country controller

--- api/src/controllers/country.controller.js
+++ api/src/controllers/country.controller.js
@@ -1,27 +1,36 @@
-const { Country, Op } = require("../db");
+const { Country, Activity, Op } = require("../db");
 
-// get all countries
-// example: /countries
 const getCountries = async (req, res) => {
-  const limit = 9;
-  let page = parseInt(req.query.page);
+  const { name, region, activity } = req.query;
 
-  if (!page || typeof page !== "number" || page < 0) page = 1;
+  // filters
+  let filters = {};
+  // /countries?name=chi
+  if (name) filters.name = { [Op.iLike]: `%${name}%` };
+
+  // /countries?region=americas
+  if (region) filters.region = { [Op.iLike]: `%${region}%` };
 
   try {
-    // data in db
-    const countriesDB = await Country.findAndCountAll({
-      attributes: ["id", "name", "flag", "region"],
-      offset: page * limit,
-      limit,
+    const countries = await Country.findAndCountAll({
+      attributes: ["id", "name", "flag", "region", "population"],
+      include: [
+        {
+          model: Activity,
+          attributes: ["id", "name"],
+          where: activity ? { name: activity } : null,
+        },
+      ],
+      where: Object.keys(filters).length ? filters : null,
     });
 
-    if (countriesDB.count) {
-      res.send({
-        totalPages: Math.ceil(countriesDB.count / limit),
-        results: countriesDB.rows,
-      });
+    if (countries.count === 0) {
+      return res
+        .status(404)
+        .send({ status: 404, msg: "The country does not exists." });
     }
+
+    res.send(countries.rows);
   } catch (err) {
     console.log(err);
     res.status(500).send({ msg: "Ups! An error has ocurred." });
@@ -44,7 +53,7 @@ const getCountryById = async (req, res) => {
         msg: "The country does not exist.",
       });
 
-    res.json(country);
+    res.send(country);
   } catch (err) {
     console.log(err);
     res.status(500).send({ msg: "Ups! An error has ocurred." });
@@ -53,28 +62,40 @@ const getCountryById = async (req, res) => {
 
 // get a various country
 // example: /countries?name=chi
-const getCountriesByName = async (req, res, next) => {
-  const { name } = req.query;
+// const getCountriesByName = async (req, res, next) => {
+//   const { name } = req.query;
 
-  if (!name) return next();
+//   if (!name) return next();
 
-  try {
-    const countries = await Country.findAll({
-      attributes: ["id", "name", "flag", "region"],
-      where: {
-        name: { [Op.iLike]: `%${name}%` },
-      },
-    });
+//   try {
+//     const countries = await Country.findAll({
+//       attributes: ["id", "name", "flag", "region"],
+//       where: {
+//         name: { [Op.iLike]: `%${name}%` },
+//       },
+//       include: [
+//         {
+//           model: Activity,
+//           attributes: ["id", "name"],
+//         },
+//       ],
+//     });
 
-    res.json(countries);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send({ msg: "Ups! An error has ocurred." });
-  }
-};
+//     if (!countries.length) {
+//       return res
+//         .status(404)
+//         .send({ status: 404, msg: "The country does not exists." });
+//     }
+
+//     res.send(countries);
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).send({ msg: "Ups! An error has ocurred." });
+//   }
+// };
 
 module.exports = {
   getCountries,
   getCountryById,
-  getCountriesByName,
+  // getCountriesByName,
 };
