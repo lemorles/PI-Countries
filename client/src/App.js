765c66198138d0aa1922bf778219dbd79e4eaf2b
Leandro <leomoralesarce@gmail.com>
2022-04-07 14:34:52 -0300

    create Not Found route

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import Activity from "./components/Activity";
-import Country from "./components/Country";
-import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 import Landing from "./components/Landing";
+import Home from "./components/Home";
+import Country from "./components/Country";
+import Activity from "./components/Activity";
+import Error404 from "./components/Error404";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar />
       <Routes>
         <Route exact path={"/"} element={<Landing />} />
         <Route exact path={"/home"} element={<Home />} />
-        <Route exact path={"/activity"} element={<Activity />} />
         <Route exact path={"/country/:id"} element={<Country />} />
+        <Route exact path={"/activity"} element={<Activity />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </div>
   );
