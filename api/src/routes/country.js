d8bcc7d707ead61ea2de0e1649bcc923e592d660
Leandro <leomoralesarce@gmail.com>
2022-04-07 11:53:43 -0300

    remove getCountriesByName route

--- api/src/routes/country.js
+++ api/src/routes/country.js
@@ -3,13 +3,9 @@ const router = Router();
 const {
   getCountries,
   getCountryById,
-  getCountriesByName,
 } = require("../controllers/country.controller");
 
 // /countries
-
-// get a various country
-router.get("/", getCountriesByName);
 
 // get all countries
 router.get("/", getCountries);
